47a6b88e8ad4569201dd9d038fd1e69032b88cfb
andriy <bartush33@gmail.com>
2024-05-09 23:47:28 +0300

    hw

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    const united = firstArray.concat(secondArray);
+        if (united.length > maxLength) {
+            return united.slice(0, maxLength);
+        } else {
+            return united;
+        }
+    
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
