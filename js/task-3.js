a6fa41292abaf21e06bd6fb86d6fe8910da7faa1
Керівник відділу Луцьк <lush@kpp.corp.com>
2024-05-11 13:44:18 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     let total = [];
-    for (let i = 0; i <= numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         if (numbers[i]  > value) {
             total.push(numbers[i]);
         }     
